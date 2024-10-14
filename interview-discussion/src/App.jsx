5aa7394b50b3d0021fb82868db7e4632ae5881f7
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-10-14 17:57:17 +0530

    added interview

--- interview-discussion/src/App.jsx
+++ interview-discussion/src/App.jsx
@@ -4,16 +4,22 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState(1);
-  let multipliedValue = value * 5;
-  const multiplyBy5 = () => {
-    setValue(value + 1);
+  console.log("App Rendered", Math.random());
+  const [value, setValue] = useState([1, 2, 3]);
+  const clickMe = () => {
+    setValue([1, 2, 3]);
+    console.log("logged");
   };
+
+  // let multipliedValue = value * 5;
+  // const multiplyBy5 = () => {
+  //   setValue(value + 1);
+  // };
   return (
     <>
-      <h1>Main value: {value}</h1>
-      <button onClick={multiplyBy5}>Click to multiply by 5</button>
-      <h2>Multiplied value: {multipliedValue} </h2>
+      <h1>Main value: {value[1]}</h1>
+      <button onClick={clickMe}>Click to multiply by 5</button>
+      {/* <h2>Multiplied value: {multipliedValue} </h2> */}
     </>
   );
 }
